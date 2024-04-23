71633d81aac15334504ba17e636cb52e923b5dac
zockq1 <zockq1@gmail.com>
2024-04-24 01:28:23 +0900

    Docs: 디렉토리 구조 정의

--- README.md
+++ README.md
@@ -10,3 +10,22 @@ npm run mock
 npm run storybook
 
 [UI 문서](docs/ui.md)
+
+디렉토리
+
+효율적인 관리를 위해 디렉토리 구조를 다음과 같이 정의합니다.
+
+```
+┌─ src
+│ ├─ app
+│ │ ├─ layout # 기본 레이아웃
+│ │ ├─ page # 페이지별 세부 컴포넌트
+│ │ └─ shared # 공통 컴포넌트
+│ ├─ shared
+│ │ ├─ auth # 인증 관련 코드
+│ │ └─ hooks # react hooks
+│ ├─ mocks # 페이지
+│ ├─ styles # 기본 스타일
+│ └─ types # 타입 정의
+└─ public # 이미지등 정적 파일
+```
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from 'react';
 
-import Prefetch from '@/share/async/Prefetch';
 import Layout from '@/share/layout/Layout';
+import Prefetch from '@/share/query/Prefetch';
+import { usePrefetchCategory } from '@/share/query/test/useGetCategory';
 
-import { usePrefetchCategory } from '../../query/test/useGetCategory';
 import Category from './_component/Category';
 
 export default async function Test() {
