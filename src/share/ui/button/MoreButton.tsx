1674a0fe92345457ab34b47ed4a421f1dc205eb9
zockq1 <zockq1@gmail.com>
2024-04-26 16:54:02 +0900

    Fix: 오타 수정

--- src/share/ui/button/MoreButton.tsx
+++ src/share/ui/button/MoreButton.tsx
@@ -8,11 +8,11 @@ interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export default function MoreButton({ ...props }: IconButtonProps) {
   return (
     <button
-      className=" p-[9px]text-[11px] flex content-center items-center rounded-[10px] font-bold text-[#999999]"
+      className="flex content-center items-center rounded-[10px] p-[9px] text-[11px] font-bold text-[#999999]"
       {...props}
     >
-      <div className="text-center">더보기 </div>
-      <Image src={arrow} alt="더보기" width={22} height={22} color="#999999" />
+      <div className="text-center">더보기</div>
+      <Image src={arrow} alt="더보기" width={15} height={15} color="#999999" />
     </button>
   );
 }
