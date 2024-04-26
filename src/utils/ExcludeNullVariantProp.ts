de0ebfecc6234dad012198b23e4322981311e8f9
zockq1 <zockq1@gmail.com>
2024-04-26 17:36:00 +0900

    Fix: eslint any escape를 제거하고 args의 any만 무시하도록 변경

--- src/utils/ExcludeNullVariantProp.ts
+++ src/utils/ExcludeNullVariantProp.ts
@@ -2,6 +2,5 @@ import { VariantProps } from 'class-variance-authority';
 
 type ExcludeNull<T> = { [P in keyof T]: Exclude<T[P], null> };
 
-/* eslint-disable  @typescript-eslint/no-explicit-any */
-export type ExcludeNullVariantProp<Comp extends (...args: any) => any> =
+export type ExcludeNullVariantProp<Comp extends (...args: any[]) => string> =
   ExcludeNull<VariantProps<Comp>>;
