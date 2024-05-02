a8e6530b38e824018988ade20e62f47fa2d41d6c
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-05-02 17:05:08 +0900

    header component화 작업중 로컬 push

--- src/share/ui/sidebar/Sidebar.tsx
+++ src/share/ui/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useSelectedLayoutSegments } from 'next/navigation';
+import { usePathname, useSelectedLayoutSegments } from 'next/navigation';
 import { useEffect } from 'react';
 
 import { contentList } from '@/share/ui/header/headerList';
@@ -12,7 +12,8 @@ export default function SidebarComponent({
   sidebarList: unknown[];
 }) {
   const segments = useSelectedLayoutSegments();
-  const activeSegments = segments[segments.length - 1];
+  const router = usePathname();
+  const activeSegments = router.split('/')[2];
   const linkQuery: string[] = ['1', '2', '3', '4', '5'];
 
   useEffect(() => {
@@ -21,14 +22,17 @@ export default function SidebarComponent({
   }, []);
 
   return (
-    <div className="h-full w-full bg-gray-3 pl-4">
+    <div className="h-full w-full bg-white">
       <ul>
         {/* //TODO 테스트 리스트 추후변경*/}
-        <li>Route에 따라 변경</li>
+        <li className="pb-2 text-lg font-bold">Route에 따라 변경</li>
         {contentList.map((item, index) => {
           const isActive = activeSegments == linkQuery[index];
           return (
-            <li key={index} className={isActive ? 'bg-primary' : 'bg-gray-3'}>
+            <li
+              key={index}
+              className={`p-2 ${isActive ? 'bg-primary' : 'bg-gray-3'}`}
+            >
               <Link href={`/test/${index + 1}`}>{item.title}</Link>
             </li>
           );
