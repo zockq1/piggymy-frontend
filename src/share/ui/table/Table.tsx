aa9fec07f73d5792a9534ecfcbf24c35fe2a811a
joseph <heegu0311@gmail.com>
2024-05-11 12:19:07 +0900

    fix: #30 테이블 컴포넌트 개선 (Ant-design)

--- src/share/ui/table/Table.tsx
+++ src/share/ui/table/Table.tsx
@@ -1,101 +1,56 @@
-import { Table as AntdTable, TableProps as AntdTableProps } from 'antd';
+import {
+  ConfigProvider,
+  Table as AntdTable,
+  TableProps as AntdTableProps,
+} from 'antd';
 import * as React from 'react';
+import { ReactNode } from 'react';
 
-interface DataType {
-  key: string;
-  isUsed: string;
-  word: string;
-  quiz: string;
+interface TableProps extends AntdTableProps {
+  hasSelection: boolean;
 }
 
-const columns: AntdTableProps<DataType>['columns'] = [
-  {
-    title: '사용여부',
-    dataIndex: 'isUsed',
-    key: 'isUsed',
-    align: 'center',
-    width: 100,
-  },
-  {
-    title: '용어카드',
-    dataIndex: 'word',
-    key: 'word',
-    width: 300,
-  },
-  {
-    title: '퀴즈카드',
-    dataIndex: 'quiz',
-    key: 'quiz',
-    width: 300,
-  },
-];
-
-const data: DataType[] = [
-  {
-    key: '1',
-    isUsed: '사용중',
-    word: '관치금융',
-    quiz: '관치금융이란 ~~~~',
-  },
-  {
-    key: '2',
-    isUsed: '사용중',
-    word: '관치금융',
-    quiz: '관치금융이란 ~~~~',
-  },
-  {
-    key: '3',
-    isUsed: '사용중',
-    word: '관치금융',
-    quiz: '관치금융이란 ~~~~',
-  },
-  {
-    key: '4',
-    isUsed: '사용중',
-    word: '관치금융',
-    quiz: '관치금융이란 ~~~~',
-  },
-  {
-    key: '5',
-    isUsed: '사용중',
-    word: '관치금융',
-    quiz: '관치금융이란 ~~~~',
-  },
-  {
-    key: '6',
-    isUsed: '사용중',
-    word: '관치금융',
-    quiz: '관치금융이란 ~~~~',
-  },
-];
-
-interface TableProps {}
-
-const Table = ({ ...props }: TableProps) => {
+const Table = ({ dataSource, columns, hasSelection, ...props }: TableProps) => {
   const rowSelection = {
-    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+    onChange: (
+      selectedRowKeys: React.Key[],
+      selectedRows: { [key: string]: number | string | ReactNode }[],
+    ) => {
       console.log(
         `selectedRowKeys: ${selectedRowKeys}`,
         'selectedRows: ',
         selectedRows,
       );
     },
-    getCheckboxProps: (record: DataType) => ({
-      disabled: record.isUsed === 'Disabled User', // Column configuration not to be checked
-      name: record.isUsed,
-    }),
+    getCheckboxProps: (record: {
+      [key: string]: number | string | ReactNode;
+    }) => {
+      console.log(record);
+      return {};
+    },
   };
 
   return (
-    <AntdTable
-      columns={columns}
-      dataSource={data}
-      pagination={{ hideOnSinglePage: true }}
-      scroll={{ x: 700, y: 300 }}
-      bordered
-      rowSelection={rowSelection}
-      {...props}
-    />
+    <ConfigProvider
+      theme={{
+        components: {
+          Table: {
+            headerBg: '#F4F5F8',
+            headerBorderRadius: 0,
+            borderColor: '#CDD1D9',
+          },
+        },
+      }}
+    >
+      <AntdTable
+        columns={columns}
+        dataSource={dataSource}
+        bordered
+        rowSelection={hasSelection ? rowSelection : undefined}
+        pagination={false}
+        {...props}
+      />
+    </ConfigProvider>
   );
 };
 
