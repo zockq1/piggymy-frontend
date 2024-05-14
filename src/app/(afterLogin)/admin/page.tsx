2747a8da874d6dc57c0f9b35894d58996a516732
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-05-14 16:28:20 +0900

    local push

--- src/app/(afterLogin)/admin/page.tsx
+++ src/app/(afterLogin)/admin/page.tsx
@@ -1,37 +1,30 @@
-import Link from 'next/link';
-
-import HomeLayout from '@/share/layout/HomeLayout';
-
+// import Link from 'next/link';
+//
+// import HomeLayout from '@/share/layout/HomeLayout';
+// import Header from '@/share/layout/header/Header';
+//
 export default async function Admin() {
   return (
-    <HomeLayout>
-      <div
-        style={{
-          gridArea: 'header',
-        }}
-      >
-        <Link href="/admin">홈</Link>&nbsp;&nbsp;
-        <Link href="/admin/content/greetingMessage">콘텐츠</Link>&nbsp;&nbsp;
-        <Link href="/admin/quiz/termManagement">용어/퀴즈</Link>&nbsp;&nbsp;
-        <Link href="/admin/setting/termsOfUse">설정</Link>&nbsp;&nbsp;
-        <Link href="/admin/user/basicInfo">회원</Link>&nbsp;&nbsp;
-        <Link href="/admin/management/basicInfo">관리자</Link>
-      </div>
-      <HomeLayout.Top>
-        <div className="h-full bg-gray-5">그리팅 메시지</div>
-      </HomeLayout.Top>
-      <HomeLayout.Center>
-        <div className="h-full bg-primary">회원 의견 모아보기</div>
-      </HomeLayout.Center>
-      <HomeLayout.Bottom>
-        <div className="h-full bg-warning">최근 등록된 테마별 카드모음집</div>
-        <div className="h-full bg-gray-5">최근 등록된 용어카드</div>
-      </HomeLayout.Bottom>
-      <HomeLayout.Right>
-        <div className="h-full bg-secondary">FireBase 바로가기</div>
-        <div className="h-full bg-warning">피기미 자료 모음 바로가기</div>
-        <div className="h-full bg-secondary">공지사항</div>
-      </HomeLayout.Right>
-    </HomeLayout>
+    <>test</>
+    // <HomeLayout>
+    //       <HomeLayout.Header>
+    //         <Header user="" />
+    //       </HomeLayout.Header>
+    //       <HomeLayout.Top>
+    //         <div className="h-full bg-gray-5">그리팅 메시지</div>
+    //       </HomeLayout.Top>
+    //       <HomeLayout.Center>
+    //         <div className="h-full bg-primary">회원 의견 모아보기</div>
+    //       </HomeLayout.Center>
+    //       <HomeLayout.Bottom>
+    //         <div className="h-full bg-warning">최근 등록된 테마별 카드모음집</div>
+    //         <div className="h-full bg-gray-5">최근 등록된 용어카드</div>
+    //       </HomeLayout.Bottom>
+    //       <HomeLayout.Right>
+    //         <div className="h-full bg-secondary">FireBase 바로가기</div>
+    //         <div className="h-full bg-warning">피기미 자료 모음 바로가기</div>
+    //         <div className="h-full bg-secondary">공지사항</div>
+    //       </HomeLayout.Right>
+    //     </HomeLayout>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
     <HomeLayout>
       <HomeLayout.Header>
-        <Header user="" headerList={[]} />
+        <Header user="" />
       </HomeLayout.Header>
       <HomeLayout.Top>
         <div className="h-full bg-gray-1">top</div>
