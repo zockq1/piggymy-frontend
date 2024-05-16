6e3ee05e86cc93651f32f23abdd88190dd8cdbc2
zockq1 <zockq1@gmail.com>
2024-05-16 20:07:14 +0900

    Refactor: axios 와 쿠키만 사용해 인증 로직 구현

---
+++ src/share/auth/useLogin.ts
@@ -0,0 +1,41 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { setCookie } from 'cookies-next';
+
+import { LoginRequestModel, LoginResponseModel } from '@/type/authType';
+import Response from '@/type/responseType';
+
+import { baseURL } from '../query/fetch-config';
+
+export const login = async (loginData: LoginRequestModel) => {
+  const response = await axios.post<Response<LoginResponseModel>>(
+    `${baseURL}/api/member/login`,
+    loginData,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  return response.data;
+};
+
+export function useLogin() {
+  return useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      setCookie('accessToken', data.data.accessToken, {
+        maxAge: 6 * 60 * 60,
+      });
+      setCookie('refreshToken', data.data.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60,
+      });
+      setCookie('memberName', data.data.memberName, {
+        maxAge: 30 * 24 * 60 * 60,
+      });
+    },
+  });
+}
