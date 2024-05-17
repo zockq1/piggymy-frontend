dccf67c05054d99f89978a8b099a12dce684e6a5
zockq1 <zockq1@gmail.com>
2024-05-17 17:30:56 +0900

    Refactor: 폴더 이동

--- src/share/query/auth/useLogin.ts
+++ src/share/query/auth/useLogin.ts
@@ -4,15 +4,14 @@ import { useMutation } from '@tanstack/react-query';
 import { notification } from 'antd';
 import axios, { AxiosError } from 'axios';
 import { setCookie } from 'cookies-next';
+import { useRouter } from 'next/router';
 
 import { LoginRequestModel, LoginResponseModel } from '@/type/authType';
 import Response from '@/type/responseType';
 
-import { baseURL } from '../query/fetch-config';
-
 export const login = async (loginData: LoginRequestModel) => {
   const response = await axios.post<Response<LoginResponseModel>>(
-    `${baseURL}/api/member/login`,
+    `${process.env.NEXT_PUBLIC_BACK_API}/api/member/login`,
     loginData,
     {
       headers: {
@@ -25,6 +24,8 @@ export const login = async (loginData: LoginRequestModel) => {
 };
 
 export function useLogin() {
+  const router = useRouter();
+
   return useMutation({
     mutationFn: login,
     onSuccess: (data) => {
@@ -37,11 +38,12 @@ export function useLogin() {
       setCookie('memberName', data.data.memberName, {
         maxAge: 30 * 24 * 60 * 60,
       });
+      router.push('/admin');
     },
     onError: (error: AxiosError<Response<unknown>, unknown>) => {
       if (axios.isAxiosError(error)) {
         notification.error({
-          message: '에러 발생',
+          message: '로그인 실패',
           description: `${error.response?.data.code}: ${error.response?.data.message}`,
         });
         console.log(error.response);
