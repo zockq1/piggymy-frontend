bcbc230efb3d8e90cb32931c3692714caedae5eb
joseph <heegu0311@gmail.com>
2024-05-19 16:47:15 +0900

    fix: #2 커스텀 모달 (용어/퀴즈카드 설정) - WordBadgeVariant 제거

--- src/share/ui/badge/WordBadge.tsx
+++ src/share/ui/badge/WordBadge.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import { cva } from 'class-variance-authority';
 import { HTMLAttributes } from 'react';
 
 import { cn } from '@/utils/cn';
-
-export const WordBadgeVariants = cva('', {
-  variants: {},
-  defaultVariants: {},
-});
 
 interface WordBadgeProps extends HTMLAttributes<HTMLElement> {
   word: string;
@@ -22,7 +16,6 @@ export default function WordBadge({
   return (
     <div
       className={cn(
-        WordBadgeVariants({}),
         'inline-flex h-max w-max gap-1 whitespace-nowrap rounded bg-[#DEEBFF] px-[10px] text-center text-[11px] font-semibold leading-[22px]',
         className,
       )}
