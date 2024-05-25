ea22dfcf2e4826dd1c34acf040ebf89f18ecb49a
zockq1 <zockq1@gmail.com>
2024-05-25 19:19:18 +0900

    Fix: key 에러 수정, 중복 코드 제거

--- src/share/ui/breadcrumb/Breadcrumb.tsx
+++ src/share/ui/breadcrumb/Breadcrumb.tsx
@@ -9,23 +9,14 @@ interface BreadcrumbProps {
 function Breadcrumb({ path }: BreadcrumbProps) {
   return (
     <div className="inline-flex items-center justify-center gap-3">
-      {path.map((p, i) =>
-        i < path.length - 1 ? (
-          <>
-            <span className="font-inter text-left text-[13px] font-bold leading-5 tracking-tighter text-[#999]">
-              {p}
-            </span>
-            <Image src={arrow} alt="next" width={5} />
-          </>
-        ) : (
-          <span
-            key={'breadcrumb' + i}
-            className="font-inter text-left text-[13px] font-bold leading-5 tracking-tighter"
-          >
+      {path.map((p, i) => (
+        <div key={p} className="inline-flex items-center justify-center gap-3">
+          <span className="font-inter text-left text-[13px] font-bold leading-5 tracking-tighter text-[#999]">
             {p}
           </span>
-        ),
-      )}
+          {i < path.length - 1 && <Image src={arrow} alt="next" width={5} />}
+        </div>
+      ))}
     </div>
   );
 }
