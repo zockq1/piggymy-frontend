d8bc18ccb0c382de5daf6d1cc37173339e22917d
joseph <heegu0311@gmail.com>
2024-05-31 20:49:12 +0900

    feat: #49 퀴즈카즈 관리 페이지 UI

--- src/app/(afterLogin)/admin/quiz/quizManagement/page.tsx
+++ src/app/(afterLogin)/admin/quiz/quizManagement/page.tsx
@@ -1,20 +1,26 @@
+import React from 'react';
+
+import CardFilter from '@/app/(afterLogin)/admin/quiz/_components/CardFilter';
+import CardSearchList from '@/app/(afterLogin)/admin/quiz/_components/CardSearchList';
+import QuizCardInfoForm from '@/app/(afterLogin)/admin/quiz/_components/QuizCardInfoForm';
+import QuizPageInfo from '@/app/(afterLogin)/admin/quiz/_components/QuizPageInfo';
 import Layout from '@/share/layout/Layout';
 
 export default async function QuizManagement() {
   return (
     <>
       <Layout.Content.Full>
-        <div className=" h-[50px] bg-primary">라우팅 타이틀</div>
+        <QuizPageInfo />
       </Layout.Content.Full>
       <Layout.Content.Full>
-        <div className=" h-[100px] bg-secondary">퀴즈 카드 검색</div>
+        <CardFilter />
       </Layout.Content.Full>
 
       <Layout.Content.Left>
-        <div className=" h-[700px] bg-gray-3">퀴즈 카드 검색</div>
+        <CardSearchList />
       </Layout.Content.Left>
       <Layout.Content.Right>
-        <div className=" h-[800px] bg-warning">퀴즈 카드 폼</div>
+        <QuizCardInfoForm />
       </Layout.Content.Right>
     </>
   );
