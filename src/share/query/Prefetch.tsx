51adb75ec055a4441986edd45ebe04112f49f44b
zockq1 <zockq1@gmail.com>
2024-06-05 05:15:04 +0900

    Feat: Prefetch에서 매개변수 받을 수 있도록 수정

--- src/share/query/Prefetch.tsx
+++ src/share/query/Prefetch.tsx
@@ -5,18 +5,27 @@ import {
 } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 interface PrefetchProps {
-  prefetchQueries: ((queryClient: QueryClient) => Promise<void>)[];
+  prefetchQueries: ((queryClient: QueryClient, data?: any) => Promise<void>)[];
+  prefetchQueryDatas?: any[];
   children?: ReactNode;
 }
 
 export default async function Prefetch({
   children,
   prefetchQueries,
+  prefetchQueryDatas,
 }: PrefetchProps) {
   const queryClient = new QueryClient();
   await Promise.all(
-    prefetchQueries.map((prefetchQuery) => prefetchQuery(queryClient)),
+    prefetchQueries.map((prefetchQuery, index) =>
+      !prefetchQueryDatas
+        ? prefetchQuery(queryClient)
+        : prefetchQueryDatas[index]
+          ? prefetchQuery(queryClient, prefetchQueryDatas[index])
+          : prefetchQuery(queryClient),
+    ),
   );
   const dehydratedState = dehydrate(queryClient);
 
