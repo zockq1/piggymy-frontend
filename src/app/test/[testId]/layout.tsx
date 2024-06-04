408ed4d8ac93588cfcee428260da1ae1243ecae9
zockq1 <zockq1@gmail.com>
2024-06-04 21:34:02 +0900

    Remove: 불필요한 test 파일 제거

--- src/app/test/[testId]/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function TestLayout({ children }: { children: ReactNode }) {
-  return <div className="text-purple-0.5">asdaasdasdsd{children}</div>;
-}
