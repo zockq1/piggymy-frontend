b7f6a9e62a41bc052f732ee03308d6a1516fb6f8
joseph <heegu0311@gmail.com>
2024-06-25 10:36:52 +0900

    refactor: #49 용어/퀴즈 페이지 layout 사용 않도록 원상복구 (페이지 구조 파악을 위한 코드 가독성)

--- src/app/(afterLogin)/admin/quiz/quizManagement/page.tsx
+++ src/app/(afterLogin)/admin/quiz/quizManagement/page.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import CardFilter from '@/app/(afterLogin)/admin/quiz/_components/CardFilter';
+import PageInfo from '@/app/(afterLogin)/admin/quiz/_components/PageInfo';
+import QuizSearchList from '@/app/(afterLogin)/admin/quiz/quizManagement/_components/QuizSearchList';
+import Layout from '@/share/layout/Layout';
+
 import CreateQuiz from './_components/CreateQuiz';
 
 export default async function QuizManagement() {
-  return <CreateQuiz />;
+  return (
+    <>
+      <Layout.Content.Full>
+        <PageInfo
+          title={'퀴즈카드 관리'}
+          path={['용어 퀴즈', '퀴즈카드 관리']}
+        />
+      </Layout.Content.Full>
+      <Layout.Content.Full>
+        <CardFilter />
+      </Layout.Content.Full>
+
+      <Layout.Content.Left>
+        <QuizSearchList />
+      </Layout.Content.Left>
+      <Layout.Content.Right>
+        <CreateQuiz />
+      </Layout.Content.Right>
+    </>
+  );
 }
